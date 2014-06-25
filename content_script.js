8b118bfd6d0bf0dacc3468eb87487795c71ac290
Micah Baize <micah.baize@gmail.com>
2014-06-24 20:52:59 -0500

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,29 +1,31 @@
-var search_site;
-var pos;
 var search_words;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   
 	if (request.salutation == "time to search") {
 		$(document).ready(function(){
-			search_site = request.site;			
-			pos = request.pos;			
-			if (pos == (0 || 1))
-			{				
+			
+			if ($('meta[name=keywords]').attr("content"))
+			{
+				search_words = $('meta[name=keywords]').attr("content");
+			}
+			else if ($('meta[itemprop=description]').attr("content"))
+			{	
 				search_words = $('meta[itemprop=description]').attr("content");
 			}
-			else if (pos == 2)
-			{				
-				search_words = $('meta[name=keywords]').attr("content");				
-			}
-			else if (pos == 3)
-			{				
-				search_words = $('meta[name=Description]').attr("content");
-			}
-			else
-			{				
+			else if ($('meta[name=description]').attr("content"))
+			{
 				search_words = $('meta[name=description]').attr("content");
+			}	
+			else if ($('meta[name=Description]').attr("content"))
+			{
+				search_words = $('meta[name=Description]').attr("content");
 			}		
+			else 
+			{
+				search_words = "Site isn't currently supported. Please manually enter your search.";
+			}					
+					
 			sendResponse({shoutback: search_words});	
 		});
 	}
